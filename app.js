5c500586c8b3ff61bf07bcc6b027e56de2406d4c
Ross King <sleeping.stars@gmail.com>
2017-03-01 12:16:26 +0000

    Added jQuery to project. Redesigned toggle animation to use jQuery instead, as CSS transitions were triggering on window resize causing the menu to appear for a brief moment. Removed redundant CSS

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-var menu = document.getElementById( 'menu-s1' ),
-		toggleMenu = document.getElementById( 'toggleMenu' ),
-    wrap = document.querySelector( '.wrap' );
+var menu = $('#menu'),
+		menuToggle = $('#menuToggle'),
+    wrap = $('.wrap');
 
+// Click Toggle
+// (Function to replace deprecated .toggle jQuery method)
+$.fn.clicktoggle = function(a, b) {
+  return this.each(function() {
+    var clicked = false;
 
-toggleMenu.onclick = function() {
-	this.classList.toggle( 'active' );
-	wrap.classList.toggle( 'menu-push' );
-	menu.classList.toggle( 'menu-open' );
+    $(this).bind("click", function() {
+      if (clicked) {
+        clicked = false;
+        return b.apply(this, arguments);
+      }
+
+      clicked = true;
+      return a.apply(this, arguments);
+    });
+  });
 };
 
-window.addEventListener('resize', function(e) {
-  var width = window.innerWidth;
+// Toggle Menu
+menuToggle.clicktoggle(function(){
+	wrap.animate({left: '240px'}, 500);
+	menu.animate({left: '0px'}, 500);
+}, function(){
+	wrap.animate({left: '0px'}, 500);
+	menu.animate({left: '-240px'}, 500);
+});
+
+// Show/Hide Mobile Navigation on window resize
+$(window).resize(function() {
+  var width = $(window).width();
   if (width > 991) {
-		wrap.classList.remove('menu-push');
-		menu.classList.remove('menu-open');
+		wrap.css('left', '0px');
+		menu.css('left', '0px');
+	} else {
+		menu.css('left', '-240px');
 	}
 });
